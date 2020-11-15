0b21bde08f590b894ccd35870219a7319ad838e1
AbiNazha <abizar213@gmail.com>
2020-11-15 13:02:38 +0700

    Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:e_Masker/pages/openingscreen.dart';
+import 'package:e_Masker/pages/launcher.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "e-Masker",
-      home: OpeningPages(),
+      home: new LauncherPage(),
       // theme: ThemeData(
       //   primarySwatch: Colors.blue,
       //   visualDensity: VisualDensity.adaptivePlatformDensity,
