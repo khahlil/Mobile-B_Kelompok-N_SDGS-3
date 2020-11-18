bf18d4033bbc78b2fefae4ffe16dac270b2db081
Indefandia <nagaseayase46@gmail.com>
2020-11-18 09:27:15 +0700

    update opening

--- lib/model/m_opening.dart
+++ lib/model/m_opening.dart
@@ -1,44 +1,43 @@
-class OpeningModel{
+class OpeningModel {
   String imageAssetPath, title, desc;
 
-  OpeningModel({this.imageAssetPath,this.title,this.desc});
+  OpeningModel({this.imageAssetPath, this.title, this.desc});
 
-  String setImageAssetPath(String s){
+  String setImageAssetPath(String s) {
     return imageAssetPath = s;
   }
 
-  String setTitle(String s){
+  String setTitle(String s) {
     return title = s;
   }
 
-  String setDesc(String s){
+  String setDesc(String s) {
     return desc = s;
   }
 }
 
-
-List<OpeningModel> getSlides(){
+List<OpeningModel> getSlides() {
   List<OpeningModel> slides = new List<OpeningModel>();
   OpeningModel sliderModel = new OpeningModel();
 
   //1
-  sliderModel.setImageAssetPath("assets/images/illustration.png");
-  sliderModel.setTitle("Search");
-  sliderModel.setDesc("Discover Restaurants offering the best fast food near you on Foodwalk");
+  sliderModel.setImageAssetPath("assets/images/gambar3.png");
+  sliderModel.setTitle("Enemy");
+  sliderModel.setDesc("Ayo bersama kita lawan virus Covid-19");
   slides.add(sliderModel);
 
   //2
   sliderModel = new OpeningModel();
-  sliderModel.setImageAssetPath("assets/images/illustration2.png");
-  sliderModel.setTitle("Order");
-  sliderModel.setDesc("Our veggie plan is filled with delicious seasonal vegetables, whole grains, beautiful cheeses and vegetarian proteins");
+  sliderModel.setImageAssetPath("assets/images/gambar2.png");
+  sliderModel.setTitle("Wear Mask");
+  sliderModel.setDesc("Jangan lupa pakai masker kemanapun anda pergi, supaya tidak terjangkit virus Covid-19");
   slides.add(sliderModel);
 
   //3
   sliderModel = new OpeningModel();
-  sliderModel.setImageAssetPath("assets/images/illustration3.png");
-  sliderModel.setTitle("Eat");
-  sliderModel.setDesc("Food delivery or pickup from local restaurants, Explore restaurants that deliver near you.");
+  sliderModel.setImageAssetPath("assets/images/gambar1.png");
+  sliderModel.setTitle("Time");
+  sliderModel.setDesc("Jangan lupa selalu mengganti masker anda dengan masker yang baru dan pastinya bersih");
   slides.add(sliderModel);
 
   return slides;
