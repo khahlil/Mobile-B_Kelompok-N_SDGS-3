d8d413dc27763b96db3d7b451a2ef3be54aa004f
khahlil <khahlil.gilbran8b@gmail.com>
2020-11-28 20:48:27 +0700

    template tab view

--- lib/pages/tab.dart
+++ lib/pages/tab.dart
@@ -1,6 +1,4 @@
 import 'package:e_Masker/control/style.dart';
-import 'package:e_Masker/pages/welcome.dart';
-import 'package:e_Masker/pages/openingscreen.dart';
 import 'package:e_Masker/pages/timer.dart';
 import 'package:flutter/material.dart';
 
@@ -24,10 +22,9 @@ class _TabPagesState extends State<TabPages> {
         length: 3,
         child: Scaffold(
           appBar: AppBar(
-            // backgroundColor: Color(0xFF3F5AA6),
-            title: Text("Timer"),
+            title: menu(),
           ),
-          bottomNavigationBar: menu(),
+          // bottomNavigationBar: menu(),
           body: TabBarView(
             children: [TimerPages(), TimerPages(), TimerPages()],
           ),
@@ -38,7 +35,7 @@ class _TabPagesState extends State<TabPages> {
 
   Widget menu() {
     return Container(
-      color: Color(0xFF3F5AA6),
+      // color: Color(0xFF3F5AA6),
       child: TabBar(
         labelColor: Colors.white,
         unselectedLabelColor: Colors.white70,
@@ -47,19 +44,16 @@ class _TabPagesState extends State<TabPages> {
         indicatorColor: Colors.blue,
         tabs: [
           Tab(
-            //...page timer history
-            // text: "Transactions",
-            icon: Icon(Icons.euro_symbol),
-          ),
-          Tab(
-            //...page timer count
-            // text: "Bills",
+            //...page timer history,
             icon: Icon(Icons.assignment),
           ),
           Tab(
             //...page input total masker
-            // text: "Balance",
-            icon: Icon(Icons.account_balance_wallet),
+            icon: Icon(Icons.edit),
+          ),
+          Tab(
+            //...page timer
+            icon: Icon(Icons.alarm),
           )
         ],
       ),
